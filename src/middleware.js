179b0c08f842c4de7f06d1f59fe76886de7eba25
Intern Rayhan <rayhan.shellbeehaken@gmail.com>
2025-01-15 11:31:29 +0600

    middleware set

---
+++ src/middleware.js
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+    const publicPaths = ['/', '/signup'];
+    const currentPath = request.nextUrl.pathname;
+    const token = request.cookies.get("token")?.value || '';
+
+    if(publicPaths.includes(currentPath) && token){
+        return NextResponse.redirect(new URL('/home', request.url))
+    }
+
+    if(!publicPaths.includes(currentPath) && !token){
+        return NextResponse.redirect(new URL('/',request.url));
+    }
+
+    return NextResponse.next();
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/',
+    '/signup',
+    '/home',
+    '/profile',
+    '/users'
+  ],
+}
